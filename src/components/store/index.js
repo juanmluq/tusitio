7f795aa33a924d7956bdf75478f8acd8ad145737
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-03-09 14:38:18 -0300

    Update index.js

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -3,5 +3,4 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 // import rootReducer from "../reducer";
 
-
 export const store = createStore(composeWithDevTools(applyMiddleware(thunk)))
